20e197d9007a2c7e83a7a8a29933a7d527cc5aaf
aniketkumarray <38500011+aniketkumarray@users.noreply.github.com>
2018-06-14 00:10:23 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -13,18 +13,23 @@ root.title('My App')
 root.geometry('300x300')
 
 root.configure(background='#EEEEEE')
+
 frame=Frame(root,bg='black')
+
 frame.pack()
-global hwL
+
 hwL = Label(frame, text='GuI', bg='green', font='Times 15 bold ')
 
 hwL.pack(fill=X, side=TOP)
 
 exitB = Button(frame, text='EXIT', command=root.destroy, bg='blue', font='Times 15 bold ')
 
-changeB = Button(root, text= 'CHANGE THE TEXT', command=change, bg='yellow', font='Times 15 bold ')
+exitB.pack(fill=X)
+
+changeB = Button(frame, text= 'CHANGE THE TEXT', command=change, bg='yellow', font='Times 15 bold ')
 
 changeB.pack(side=BOTTOM)
+
 frame.mainloop()
 
 root.mainloop()
